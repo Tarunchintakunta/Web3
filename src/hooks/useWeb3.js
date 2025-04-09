c0f7b751e938ef39ecb3db0509c123399aada1d6
Tarun chintakunta <tarunchintakunta@gmail.com>
2025-04-09 08:42:02 +0530

    feat: refactor useWeb3 hook to destructure context values for improved readability

--- src/hooks/useWeb3.js
+++ src/hooks/useWeb3.js
@@ -2,5 +2,25 @@ import { useContext } from 'react';
 import { Web3Context } from '../contexts/Web3Context';
 
 export const useWeb3 = () => {
-  return useContext(Web3Context);
+  const { 
+    account, 
+    provider, 
+    signer, 
+    isConnected, 
+    chainId, 
+    networkName,
+    connectWallet, 
+    disconnectWallet 
+  } = useContext(Web3Context);
+
+  return { 
+    account, 
+    provider, 
+    signer, 
+    isConnected, 
+    chainId, 
+    networkName,
+    connectWallet, 
+    disconnectWallet 
+  };
 };
